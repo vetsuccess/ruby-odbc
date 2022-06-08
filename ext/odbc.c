a063fb9026c0f47b5fc2deeec8a0137bae813f50
nikola <nikola@deversity.net>
2022-06-08 19:53:48 +0200

    Experiment with `rb_scan_args` that can't be compiled with ruby 2.5.9

--- ext/odbc.c
+++ ext/odbc.c
@@ -4282,7 +4282,7 @@ do_option(int argc, VALUE *argv, VALUE self, int isstmt, int op)
     char *msg;
     int level = isstmt ? OPT_LEVEL_STMT : OPT_LEVEL_DBC;
 
-    rb_scan_args(argc, argv, (op == -1) ? "11" : "01", &val, &val2);
+    rb_scan_args(argc, argv, "11", &val, &val2);
     if (isstmt) {
 	Data_Get_Struct(self, STMT, q);
 	if (q->dbc == Qnil) {
